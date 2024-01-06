31448313a72a8693455b994a12d41a41a177f655
Trung <hvthn126@gmail.com>
2024-01-06 14:33:04 +0700

    tao api dang nhap tren front end

---
+++ src/main/resources/static/js/custom.js
@@ -0,0 +1,23 @@
+$(document).ready(function() {
+    
+    $('#signin').click(function() {
+        var email = $("#email").val()
+        var password = $("#password").val()
+        console.log("gia tri " + email)
+        $.ajax({
+            method: "POST",
+            url: "http://localhost:8080/login/signin",
+            data: {
+                 username: email, 
+                 password: password 
+                }
+          })
+            .done(function( msg ) {
+                console.log(email)
+                if(msg.data)
+                    window.location.href = "./index.html"
+                else 
+                    alert("that bai");
+            });
+    });
+});
